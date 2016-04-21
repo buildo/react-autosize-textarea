8a37b78f71fc68cbc5fe0a4a1e132a7997600057
Giovanni Gonzaga <giovanni@buildo.io>
2016-04-21 12:28:47 +0200

    fix tests

--- test/tests/TextareaAutosize-test.js
+++ test/tests/TextareaAutosize-test.js
@@ -1,51 +1,30 @@
-import React from 'react/addons';
-const TestUtils = React.addons.TestUtils;
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
 import expect from 'expect';
 import TextareaAutosize from '../../lib';
 
 
 const renderTextarea = () => {
-  const component =
-    <div>
-      <TextareaAutosize className='textarea-autosize' />
-    </div>;
-  const textareaWrapper = TestUtils.renderIntoDocument(component);
-  return textareaWrapper;
+  const component = <TextareaAutosize className='textarea-autosize' />;
+  const textarea = TestUtils.renderIntoDocument(component);
+  return textarea;
 };
 
 describe('TextareaAutosize', function() {
 
   it('should be displayed', function() {
-    const textareaWrapper = renderTextarea();
-    const textarea = TestUtils.scryRenderedDOMComponentsWithClass(textareaWrapper, 'textarea-autosize');
-    expect(textarea.length).toBe(1, 'textarea is not displayed');
+    const textarea = renderTextarea();
+    const txt = TestUtils.scryRenderedDOMComponentsWithClass(textarea, 'textarea-autosize');
+    expect(txt.length).toBe(1, 'textarea is not displayed');
   });
 
   it('should display initial value', function() {
     const initialValue = 'Initial Value'
-    const component =
-      <div>
-        <TextareaAutosize className='textarea-autosize' defaultValue={initialValue} />
-      </div>;
-    const textareaWrapper = TestUtils.renderIntoDocument(component);
-    const textarea = TestUtils.findRenderedDOMComponentWithClass(textareaWrapper, 'textarea-autosize');
-    const value = textarea.getDOMNode().innerHTML;
+    const component = <TextareaAutosize className='textarea-autosize' defaultValue={initialValue} />;
+    const textarea = TestUtils.renderIntoDocument(component);
+    const txt = TestUtils.findRenderedDOMComponentWithClass(textarea, 'textarea-autosize');
+    const value = txt.innerHTML;
     expect(value).toBe(initialValue, 'intial value is not displayed correctly');
   });
-
-  // it('should resize correctly based on initial value', function() {
-  //   const initialValue = '\n\n\n\n\n\n\n\n\n\nInitial Value'
-  //   const component =
-  //     <div>
-  //       <TextareaAutosize className='textarea-autosize' defaultValue={initialValue} />
-  //     </div>;
-  //   const textareaWrapper = TestUtils.renderIntoDocument(component);
-  //   const textarea = TestUtils.findRenderedDOMComponentWithClass(textareaWrapper, 'textarea-autosize').getDOMNode();
-  //   React.addons.TestUtils.Simulate.click(textarea);
-  //   console.log(textarea.style.height);
-  //   console.log(textarea.clientHeight);
-  //   // const value = textarea.getDOMNode().innerHTML;
-  //   // expect(value).toBe(initialValue, 'intial value is not displayed correctly');
-  // });
 
 });
