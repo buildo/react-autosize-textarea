5cb2a0a98bf32b75d19455f552c24d796cc05e1a
Francesco Cioria <firefelix@gmail.com>
2017-07-03 16:11:09 +0200

    fix tests

--- test/tests/TextareaAutosize-test.js
+++ test/tests/TextareaAutosize-test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TestUtils from 'react-dom/test-utils';
 import expect from 'expect';
-import TextareaAutosize from '../../index';
+import TextareaAutosize from '../../lib';
 
 
 const renderTextarea = () => {
