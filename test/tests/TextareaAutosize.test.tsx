5e25d73aefe972b4ac2bb67f5b4c9256854c3e25
Francesco Cioria <firefelix@gmail.com>
2017-12-05 17:19:18 +0100

    add test for build script

--- test/tests/TextareaAutosize.test.tsx
+++ test/tests/TextareaAutosize.test.tsx
@@ -1,3 +1,6 @@
+import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import TextareaAutosize from '../../src';
@@ -12,3 +15,12 @@ describe('TextareaAutosize', () => {
   });
 
 });
+
+describe('build', () => {
+
+  it('build script generates every needed file', () => {
+    execSync('npm run build')
+    expect(fs.readdirSync(path.resolve(__dirname, '../../lib'))).toMatchSnapshot()
+  })
+
+})
